8ef782d874b28f0b1fc02c1edac7d60a7dda878c
Akos Stegner <a.stegner@mito.hu>
2022-04-15 17:18:02 +0200

    Add contact us

--- components/ProfileSection.vue
+++ components/ProfileSection.vue
@@ -6,10 +6,10 @@
         <h1 class="px-10 text-3xl font-bold">About me</h1>
         <p class="px-10 mt-5 text-base text-gray-500 sm:text-lg md:text-lg">
           My name is Niki S. Kunos and I am a cake decorator in IRL. In the last
-          10 years my cakes proud to be featured on Huffington Post Wedding,
+          10 years my cakes proud to be featured on <b>Huffington Post Wedding,
           Ruffled Blog,The Modern Wedding Magazine (20th-anniversary edition)
           Insyle Magazine, Glamour Mag and I was finalist “Decorator of the
-          Year!” on Australian Cake Decorating Network (2014) but most
+          Year!”</b> on Australian Cake Decorating Network (2014) but most
           importantly, I have been able to feed over 10,000 happy people with
           sweet bits.
         </p>
@@ -17,15 +17,15 @@
           Here in WEB3, it is amazing that i could combine cake decorating with
           my other passion, illustration.
         </p>
-        <h1 class="px-10 mt-10 text-xl">Niki S. Kunos</h1>
+        <h1 class="px-10 mt-10 text-base font-bold">Niki S. Kunos</h1>
         <h1 class="px-10 text-base">Artist/Founder</h1>
       </div>
-      <div class="w-full md:w-1/3 flex flex-wrap">
+      <div class="w-full md:w-1/3">
         <div class="grid grid-cols-3">
           <div class="col-span-3">
             <img
               src="https://scontent.fbud7-3.fna.fbcdn.net/v/t31.18172-8/1926191_10152658947375904_3541205336929530431_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=4vkZwlETCGEAX-G8JDY&_nc_ht=scontent.fbud7-3.fna&oh=00_AT-0jJc1XC0YfhqmvsvCY_2H7U40Y8VnAiSEsgdBNv0TFw&oe=6280589B"
-              class="object-cover"
+              class="object-cover aspect-video"
             />
           </div>
           <div class="">
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'roadmap-pattern': "url('../img/pattern-wave.svg')",
+        wave: "url('../img/wave.svg')",
       },
     },
   },
