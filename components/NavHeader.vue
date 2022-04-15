3b8d8b3272f9ff5e3a3d9e75e16585989b3c0678
Akos Stegner <a.stegner@mito.hu>
2022-04-15 08:05:18 +0200

    First commit

---
+++ components/NavHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="px-10 py-5 font-bold text-xl">NikiKunos.com</nav>
+</template>
+
+<script>
+export default {
+  name: 'NavHeader',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="max-w-7xl mx-auto">
+    <NavHeader />
+    <HeroSection />
+    <ProfileSection />
+    <FeatureSection />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
