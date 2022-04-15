483b8f73f351e006888ac27ce64ca3cf244d48d3
Akos Stegner <a.stegner@mito.hu>
2022-04-15 08:05:18 +0200

    First commit

---
+++ components/ProfileSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="bg-indigo-600 rounded-xl flex">
+    <div class="py-10">
+      <p class="px-10 mt-5 text-base text-indigo-200 sm:text-lg md:text-lg">
+        My name is Niki S. Kunos and I am a cake decorator in IRL. In the last
+        10 years my cakes proud to be featured on Huffington Post Wedding,
+        Ruffled Blog,The Modern Wedding Magazine (20th-anniversary edition)
+        Insyle Magazine, Glamour Mag and I was finalist “Decorator of the Year!”
+        on Australian Cake Decorating Network (2014) but most importantly, I
+        have been able to feed over 10,000 happy people with sweet bits.
+      </p>
+      <p class="px-10 mt-3 text-base text-indigo-200 sm:text-lg md:text-lg">
+        Here in WEB3, it is amazing that i could combine cake decorating with my
+        other passion, illustration.
+      </p>
+      <h1 class="px-10 text-white text-2xl">Niki S. Kunos</h1>
+      <h1 class="px-10 text-white text-xl">Artist/Founder</h1>
+    </div>
+    <div class="w-10/12">
+      <img
+        src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ProfileSection',
+}
+</script>
