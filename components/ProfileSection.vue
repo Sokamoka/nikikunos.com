930663489f49220827c0385b509526fa2a85ec93
Akos Stegner <a.stegner@mito.hu>
2022-04-15 13:39:29 +0200

    Develop site

--- components/ProfileSection.vue
+++ components/ProfileSection.vue
@@ -1,25 +1,70 @@
 <template>
-  <section class="bg-indigo-600 rounded-xl flex">
-    <div class="py-10">
-      <p class="px-10 mt-5 text-base text-indigo-200 sm:text-lg md:text-lg">
-        My name is Niki S. Kunos and I am a cake decorator in IRL. In the last
-        10 years my cakes proud to be featured on Huffington Post Wedding,
-        Ruffled Blog,The Modern Wedding Magazine (20th-anniversary edition)
-        Insyle Magazine, Glamour Mag and I was finalist “Decorator of the Year!”
-        on Australian Cake Decorating Network (2014) but most importantly, I
-        have been able to feed over 10,000 happy people with sweet bits.
-      </p>
-      <p class="px-10 mt-3 text-base text-indigo-200 sm:text-lg md:text-lg">
-        Here in WEB3, it is amazing that i could combine cake decorating with my
-        other passion, illustration.
-      </p>
-      <h1 class="px-10 text-white text-2xl">Niki S. Kunos</h1>
-      <h1 class="px-10 text-white text-xl">Artist/Founder</h1>
-    </div>
-    <div class="w-10/12">
-      <img
-        src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
-      />
+  <section class="">
+    <div class="max-w-7xl mx-auto flex flex-col md:flex-row">
+      <div class="w-full md:w-1/2 py-5">
+        <div class="h-1 bg-indigo-500 w-48 mb-8 mx-10"></div>
+        <h1 class="px-10 text-3xl font-bold">About me</h1>
+        <p class="px-10 mt-5 text-base text-gray-500 sm:text-lg md:text-lg">
+          My name is Niki S. Kunos and I am a cake decorator in IRL. In the last
+          10 years my cakes proud to be featured on Huffington Post Wedding,
+          Ruffled Blog,The Modern Wedding Magazine (20th-anniversary edition)
+          Insyle Magazine, Glamour Mag and I was finalist “Decorator of the
+          Year!” on Australian Cake Decorating Network (2014) but most
+          importantly, I have been able to feed over 10,000 happy people with
+          sweet bits.
+        </p>
+        <p class="px-10 mt-3 text-base text-gray-500 sm:text-lg md:text-lg">
+          Here in WEB3, it is amazing that i could combine cake decorating with
+          my other passion, illustration.
+        </p>
+        <h1 class="px-10 mt-10 text-xl">Niki S. Kunos</h1>
+        <h1 class="px-10 text-base">Artist/Founder</h1>
+      </div>
+      <div class="w-full md:w-1/2 flex flex-wrap">
+        <div class="grid grid-cols-3">
+          <div class="col-span-3">
+            <img
+              src="https://scontent.fbud7-3.fna.fbcdn.net/v/t31.18172-8/1926191_10152658947375904_3541205336929530431_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=174925&_nc_ohc=4vkZwlETCGEAX-G8JDY&_nc_ht=scontent.fbud7-3.fna&oh=00_AT-0jJc1XC0YfhqmvsvCY_2H7U40Y8VnAiSEsgdBNv0TFw&oe=6280589B"
+              class="object-cover"
+            />
+          </div>
+          <div class="">
+            <img
+              src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+            />
+          </div>
+          <div class="">
+            <img
+              src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+            />
+          </div>
+          <div class="">
+            <img
+              src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+            />
+          </div>
+        </div>
+        <!-- <div class="w-full">
+          <img
+            src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+          />
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+          />
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+          />
+        </div>
+        <div class="w-1/3">
+          <img
+            src="https://shuffle.dev/pstls-assets/images/headers/header-1-woman.jpg"
+          />
+        </div> -->
+      </div>
     </div>
   </section>
 </template>
---
+++ components/RoadmapItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <div class="flex max-w-7xl mx-auto">
+      <div class="w-1/2">
+        <div class="h-1 bg-indigo-500 w-48 mb-8 mx-10"></div>
+        <h1 class="px-10 text-3xl font-bold">“Lets celebrate with cake”</h1>
+        <p class="px-10 mt-5 text-base text-gray-500 sm:text-lg md:text-lg">
+          “Lets celebrate with cake”( 1/1) : includes only custom made cakes from any “ Cake Club Collection. (only 150 pieces) Sending a print to the owners.
+        </p>
+      </div>
+      <div class="w-1/2">B</div>
+    </div>
+  </section>
+</template>
