6939c248561e3c0b1003f2b27cc7625d010848e4
Akos Stegner <a.stegner@mito.hu>
2022-04-16 15:27:57 +0200

    Set header z-index and add opensea link

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -21,8 +21,9 @@
       <div class="mt-8">
         <div class="rounded-md shadow">
           <a
-            href="#"
+            href="https://opensea.io/collection/lets-celebrate-with-cake"
             class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+            target="_blank"
           >
             Taste your Cake
           </a>
