6276efd94c49d0d757ff7b8b7236e92f0edc22e2
Akos Stegner <a.stegner@mito.hu>
2022-06-07 19:09:20 +0200

    Add created tab link

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -24,7 +24,7 @@
       >
         <div class="rounded-md shadow">
           <a
-            href="https://opensea.io/cakeclubNFT"
+            href="https://opensea.io/cakeclubNFT?tab=created"
             class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-rose-600 hover:bg-rose-700 md:py-4 md:text-lg md:px-10"
             target="_blank"
           >
